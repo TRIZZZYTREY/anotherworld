56f038d32d8bb2a2809bc47ad681e4e03eab4d26
Khai Quang Nguyen <kqn001@bucknell.edu>
2017-11-29 20:47:41 -0500

    done

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ startExperienteBtn.onclick = function() {
   document.getElementsByTagName("a-scene")[0].style.zIndex = "auto";
 };
 
-var angel_1_position = [{ x: -0.9, y: 0, z: -10.4 }, { x: -0.9, y: 0, z: -4.6 }];
+var angel_1_position = [
+  { x: -0.9, y: 0, z: -10.4 },
+  { x: -0.9, y: 0, z: -4.6 }
+];
 var angel_2_position = [{ x: 1, y: 0, z: 2 }, { x: 1, y: 0, z: -4.3 }];
 var angel_1_in_view = true;
 var angel_2_in_view = false;
 var time_angel_1 = 0;
 var time_angel_2 = 0;
+var scene = document.getElementById("scene");
 AFRAME.registerComponent("listener", {
   tick: function() {
     var angel_1 = document.getElementById("angel_1");
@@ -19,7 +23,7 @@ AFRAME.registerComponent("listener", {
     //   get head position of view
     rotation = Math.abs((rotation.y + 360) % 360);
     this.el.setAttribute("rotation.y", rotation);
-    if (rotation > 80 && rotation < 290) {
+    if (rotation > 100 && rotation < 260) {
       if (angel_1_in_view == true) {
         angel_1_in_view = false;
         var pos = angel_1_position.shift();
@@ -28,8 +32,14 @@ AFRAME.registerComponent("listener", {
         time_angel_1++;
         if (time_angel_1 == 2) {
           //   make angel bigger
-          angel_1.setAttribute("scale", "0.02 0.02 0.02");
-          angel_1.setAttribute("material","color: #555555; metalness:.1; roughness:1");
+          angel_1.setAttribute("scale", "0.018 0.018 0.018");
+          angel_1.setAttribute(
+            "material",
+            "color: #555555; metalness:.1; roughness:1"
+          );
+          document.getElementsByTagName("a-scene")[0].style.opacity = "0";
+          var scare_sound = document.getElementById("scare_sound");
+          scare_sound.components.sound.playSound();
         }
       }
     } else {
@@ -42,8 +52,12 @@ AFRAME.registerComponent("listener", {
         angel_2.setAttribute("position", pos);
         time_angel_2++;
         if (time_angel_2 == 2) {
-          angel_2.setAttribute("scale", "0.02 0.02 0.02");
-          angel_2.setAttribute("material", "color: #555555; metalness:.1; roughness:1");
+          angel_2.setAttribute("scale", "0.018 0.018 0.018");
+          angel_2.setAttribute(
+            "material",
+            "color: #555555; metalness:.1; roughness:1"
+          );
+          document.getElementsByTagName("a-scene")[0].style.opacity = "0";
         }
       }
     } else {
